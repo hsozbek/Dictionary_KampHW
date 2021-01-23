08cf27c51542d99442fdb1e6ec0ef87f76d10dff
hsozbek <hselcukozbek@hotmail.com>
2021-01-23 13:48:24 +0300

    Files loaded

---
+++ DictionaryKampHW/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryKampHW
+{
+    class MyDictionary<T1,T2>
+    {
+        private T1[] _arrayKey;
+        private T2[] _arrayValue;
+        public MyDictionary()
+        {
+            _arrayKey = new T1[0];
+            _arrayValue = new T2[0];
+        }
+        public void Add(T1 key,T2 value)
+        {
+            T1[] tempArrayKey;
+            T2[] tempArrayValue;
+            tempArrayKey = _arrayKey;
+            tempArrayValue = _arrayValue;
+            _arrayKey = new T1[tempArrayKey.Length+1];
+            _arrayValue = new T2[tempArrayValue.Length+1];
+            
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                _arrayKey[i] = tempArrayKey[i];
+
+            }
+
+            _arrayKey[_arrayKey.Length - 1] = key;
+
+            for (int i = 0; i < tempArrayValue.Length; i++)
+            {
+                _arrayValue[i] = tempArrayValue[i];
+
+            }
+
+            _arrayValue[_arrayValue.Length - 1] = value;
+
+
+        }
+    }
+}
---
+++ DictionaryKampHW/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryKampHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+            dictionary.Add("book", "kitap");
+            Console.WriteLine();
+
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+        }
+    }
+}
